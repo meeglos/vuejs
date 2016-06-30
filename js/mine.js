0348ff40aff48e0625c809ea384f624d5f864536
Miguel Rodriguez <miglos@hotmail.es>
2016-06-30 02:29:34 +0200

    Final template + partial css

---
+++ js/mine.js
@@ -0,0 +1,17 @@
+/*
+* @Author: Miguel Rodriguez
+* @Date:   2016-06-30 00:42:57
+* @Last Modified by:   Miguel Rodriguez
+* @Last Modified time: 2016-06-30 00:50:02
+*/
+
+'use strict';
+$('#collapseOne').collapse({
+  toggle: false
+});
+$('#collapseTwo').collapse({
+  toggle: false
+});
+$('#collapseThree').collapse({
+  toggle: false
+});
